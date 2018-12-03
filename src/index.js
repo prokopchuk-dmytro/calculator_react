e0e94f436303699fa1d6e2e51694c1e353675262
Dima Prokopchuk <prokopchuk.dima@gmail.com>
2018-12-03 12:37:45 +0200

    import

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./component/App";
 import "./index.css";
+import Big from "big.js";
 
 ReactDOM.render(<App />, document.getElementById("root"));
